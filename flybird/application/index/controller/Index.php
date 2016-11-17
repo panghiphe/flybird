c073aec69615ce148803c19e88744820961273e9
mickycheng@qq.com <github!8>
2016-11-17 17:53:26 +0800

    新增框架

---
+++ flybird/application/index/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\index\controller;
+
+use app\initcore\Birdcore;
+
+class Index extends Birdcore
+{
+
+
+    /**
+     * fly bird 首页
+     */
+    public function hello(){
+
+        return $this->fetch('hello');
+    }
+}
---
+++ flybird/application/initcore/Birdcore.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moon
+ * Date: 2016/11/17
+ * Time: 下午5:50
+ * 核心类
+ */
+namespace app\initcore;
+
+
+use think\Controller;
+
+class Birdcore extends Controller{
+
+
+
+
+}
