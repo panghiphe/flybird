b0afac0440396876955b5f790e4aeef24fdcaea5
mickycheng@qq.com <github!8>
2016-11-29 08:33:49 +0800

    修改bug路由

--- flybird/application/route.php
+++ flybird/application/route.php
@@ -14,7 +14,7 @@ use think\Route;
 Route::group('bird', function () {
     Route::group('game', function () {
         Route::rule('start', 'index/game/play');  // 开始游戏
-        Route::rule('end','index/game/edn');   //游戏玩完
+        Route::rule('end','index/game/end');   //游戏玩完
         Route::rule('rank','index/game/rank');  // 游戏排名
     });
 });
