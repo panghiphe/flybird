29aac68a1148b5f55f2788fb68032438eec208d9
panghiphe <kkyyaaaa@163.com>
2016-12-07 17:53:44 +0800

    修改游戏主题风格

--- flybird/public/bird/js/load.js
+++ flybird/public/bird/js/load.js
@@ -2,10 +2,15 @@ var load_state = {
 //预加载游戏资源
     preload: function() { 
         this.game.load.image('bg','/bird/image/bg.png');
+        this.game.load.image('menubg','/bird/image/bg.jpg');
+        this.game.load.image('title','/bird/image/title.png');
+        this.game.load.image('startbtn','/bird/image/start-btn.png');
+        this.game.load.image('rulebtn','/bird/image/rule-btn.png');
+        this.game.load.image('rankbtn','/bird/image/rank-btn.png');
 		this.game.load.image('bo','/bird/image/back.png');
 		this.game.load.image('menu','/bird/image/menu.png');
 		this.game.load.image('ready','/bird/image/ready.png');
-        this.game.load.image('bird', '/bird/image/bird.png');  
+        this.game.load.spritesheet('bird', '/bird/image/bird.png', 46, 34);
         this.game.load.image('pipe', '/bird/image/pipe.png');  
         this.game.load.audio('jump', '/bird/image/jump.wav');
 		this.game.load.audio('dead','/bird/image/dead.wav');
--- flybird/public/bird/js/menu.js
+++ flybird/public/bird/js/menu.js
@@ -4,46 +4,69 @@ var menu_state={
 		var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);		//定义接受按键消息变量
 		space_key.onDown.add(this.start,this);		//按键按下时调用start()函数
 		var t = this;
-		this.game.input.touch.onTouchStart = function(){
-            t.start();
+		this.game.input.touch.onTouchStart = function(ev){
+			var evPos = {
+				x: ev.touches[0].clientX,
+				y: ev.touches[0].clientY
+			}
+			
+			if(isPointInBounds(evPos, t.rankBtn.getBounds()))
+			{
+				t.show_rank();
+			}
+			else if(isPointInBounds(evPos, t.startBtn.getBounds()))
+			{
+				t.start();
+			}
+			else if(isPointInBounds(evPos, t.ruleBtn.getBounds()))
+			{
+				alert("rule");
+			}
+            
         }
 		
 		var style = {font: "30px Arial",fill: "#FFFFFF"};		//定义游戏操作说明文字风格
 		var x = game.world.width/2,y = game.world.height/2;		//定义坐标变量x,y，(x,y)为game.world中心
 		
-		this.bg = this.game.add.sprite(0,0,'bg');		//在game.world中坐标(0,0)处画出预加载游戏资源，背景图片
-		this.bg.width = game.world.width;
-		this.bg.height = game.world.height;
-		this.bo = this.game.add.sprite(0,0,'bo');		//也是背景图片，我的背景由两个背景透明的图片组合而成，其实可以合二为一
-		this.bo.width = game.world.width;
-		this.bo.height = game.world.height;
-		//this.menu = this.game.add.sprite(0,0,'menu');		//加载menu图片
-		this.bird = this.game.add.sprite(x,y-60,'bird');		//载入即将闯荡管子世界的Bird
-		this.bird.anchor.setTo(0.5,0.5);
-		var gameName = this.game.add.text(x,y-118,"Flappy Bra",style);		//定义显示文本变量,并在game.world显示，参数(坐标,显示文本,文本风格)
-		gameName.anchor.setTo(0.5,0.5);
-		var text = this.game.add.text(x,y,"Touch to start!",style);		//定义显示文本变量,并在game.world显示，参数(坐标,显示文本,文本风格)
-		text.anchor.setTo(0.5,0.5);		//
+		this.bg = this.game.add.sprite(0,0,'menubg');		//在game.world中坐标(0,0)处画出预加载游戏资源，背景图片
+		/*this.bg.width = game.world.width;
+		this.bg.height = game.world.height;*/
+		var scale = game.world.width / this.bg.width;
+		this.bg.scale.x = game.world.width / this.bg.width;
+		this.bg.scale.y = game.world.height / this.bg.height;
 		
-		var rankBtn = this.game.add.button(x, y-180, "RANK", this.show_rank);
-		rankBtn.anchor.setTo(0.5,0.5);
-		rankBtn.tint = "0xdddddd";
-		rankBtn.width = 100;
-		rankBtn.height = 30;
-		//rankBtn.texture = ""
+		this.titleImg = this.game.add.sprite(x,game.world.height * 0.15,'title');
+		this.titleImg.scale.x = game.world.width * 0.8 / this.titleImg.width;
+		this.titleImg.scale.y = this.titleImg.scale.x;
+		this.titleImg.anchor.setTo(0.5,0);
+		
+        this.bird = this.game.add.sprite(game.world.width * 0.14, game.world.height * 0.4, 'bird');
+        //game.physics.arcade.enable(this.bird);
+        this.bird.scale.x = game.world.width * 0.184 / this.bird.width;
+		this.bird.scale.y = this.bird.scale.x;
+        this.bird.animations.add('fly', [0, 1, 2], 10, true);
+        this.bird.animations.play('fly');
+        
+		this.startBtn = this.game.add.button(x, this.titleImg.position.y + this.titleImg.height + 50, "startbtn", this.show_rank);
+        var btnScale = game.world.width * 0.58 / this.startBtn.width;
+        this.startBtn.scale.x = btnScale;
+		this.startBtn.scale.y = btnScale;
+		this.startBtn.anchor.setTo(0.5,0.5);
+		
+		this.ruleBtn = this.game.add.button(x, this.startBtn.position.y + this.startBtn.height + 10, "rulebtn", this.show_rank);
+        this.ruleBtn.scale.x = btnScale;
+		this.ruleBtn.scale.y = btnScale;
+		this.ruleBtn.anchor.setTo(0.5,0.5);
+		
+		this.rankBtn = this.game.add.button(x, this.ruleBtn.position.y + this.ruleBtn.height + 10, "rankbtn", this.show_rank);
+        this.rankBtn.scale.x = btnScale;
+		this.rankBtn.scale.y = btnScale;
+		this.rankBtn.anchor.setTo(0.5,0.5);
+		
+		//$(".opt-btn-con").fadeIn();
 	},
 	show_rank: function() {
-		$.ajax({
-    		url: "/bird/game/rank",
-    		type: "post",
-    		dataType: "json",
-    		success: function(data) {
-    			
-    		},
-    		error: function() {
-    			
-    		}
-    	});
+		alert("rank");
 	},
 	start:function(){
 		this.game.state.start('ready');		//调用start()函数后进入'ready'state
