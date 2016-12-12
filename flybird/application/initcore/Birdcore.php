a223eca473e97c1315a82348b0b8eb3bab7d8115
mickycheng@qq.com <github!8>
2016-12-12 22:57:15 +0800

    hello world

--- flybird/application/initcore/Birdcore.php
+++ flybird/application/initcore/Birdcore.php
@@ -36,6 +36,7 @@ class Birdcore extends Controller{
                 session('woaap_code', $_GET['code']);
                 $logs['info'] = '调到woa自动登录接口！';
                 $logs['code'] = $_GET['code'];
+                $logs['url'] = $_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI];
                 $logs['file'] = __FILE__;
                 $logs['line'] = __LINE__;
                 \app\addon\Applog::appLog('logs', $logs);
